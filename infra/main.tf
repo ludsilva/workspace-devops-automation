d995834309c9fc40a0e6a509d0791f9e1a84205a
Lud Silva <ludsilvapires@gmail.com>
2026-06-21 20:07:29 -0300

    fix: azure sku

--- infra/main.tf
+++ infra/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_public_ip" "public_ip" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
-  sku                 = "Basic"
+  sku                 = "Standard"
 }
 
 
@@ -83,7 +83,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                            = "vm-automation"
   resource_group_name             = azurerm_resource_group.rg.name
   location                        = azurerm_resource_group.rg.location
-  size                            = "Standard_B1s"
+  size                            = "Standard_D2s_v7"
   admin_username                  = "azureuser"
   admin_password                  = var.admin_password
   disable_password_authentication = false
